7fe221aa1111b4a2664e2a7fbc662013322767ce
Uwe Stieber <uwe.stieber@windriver.com>
2011-12-14 19:07:36 +0100

    Target Explorer: Fulfill base plug-in structure for TCF console UI plug-in

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.ui.console/src/org/eclipse/tcf/te/tcf/ui/console/activator/UIPlugin.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.ui.console/src/org/eclipse/tcf/te/tcf/ui/console/activator/UIPlugin.java
@@ -1,5 +1,9 @@
 package org.eclipse.tcf.te.tcf.ui.console.activator;
 
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.tcf.te.ui.jface.images.AbstractImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -7,35 +11,13 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class UIPlugin extends AbstractUIPlugin {
-
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.tcf.te.tcf.ui.console"; //$NON-NLS-1$
-
 	// The shared instance
 	private static UIPlugin plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public UIPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
@@ -47,4 +29,84 @@ public class UIPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	/**
+	 * Convenience method which returns the unique identifier of this plugin.
+	 */
+	public static String getUniqueIdentifier() {
+		if (getDefault() != null && getDefault().getBundle() != null) {
+			return getDefault().getBundle().getSymbolicName();
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+    public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+    public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#initializeImageRegistry(org.eclipse.jface.resource.ImageRegistry)
+	 */
+	@Override
+	protected void initializeImageRegistry(ImageRegistry registry) {
+	}
+
+	/**
+	 * Loads the image registered under the specified key from the image
+	 * registry and returns the <code>Image</code> object instance.
+	 *
+	 * @param key The key the image is registered with.
+	 * @return The <code>Image</code> object instance or <code>null</code>.
+	 */
+	public static Image getImage(String key) {
+		return getDefault().getImageRegistry().get(key);
+	}
+
+	/**
+	 * Loads the image registered under the specified key from the image
+	 * registry and returns the <code>ImageDescriptor</code> object instance.
+	 *
+	 * @param key The key the image is registered with.
+	 * @return The <code>ImageDescriptor</code> object instance or <code>null</code>.
+	 */
+	public static ImageDescriptor getImageDescriptor(String key) {
+		return getDefault().getImageRegistry().getDescriptor(key);
+	}
+
+	/**
+	 * Loads the image given by the specified image descriptor from the image
+	 * registry. If the image has been loaded ones before already, the cached
+	 * <code>Image</code> object instance is returned. Otherwise, the <code>
+	 * Image</code> object instance will be created and cached before returned.
+	 *
+	 * @param descriptor The image descriptor.
+	 * @return The corresponding <code>Image</code> object instance or <code>null</code>.
+	 */
+	public static Image getSharedImage(AbstractImageDescriptor descriptor) {
+		ImageRegistry registry = getDefault().getImageRegistry();
+
+		String imageKey = descriptor.getDecriptorKey();
+		Image image = registry.get(imageKey);
+		if (image == null) {
+			registry.put(imageKey, descriptor);
+			image = registry.get(imageKey);
+		}
+
+		return image;
+	}
 }
