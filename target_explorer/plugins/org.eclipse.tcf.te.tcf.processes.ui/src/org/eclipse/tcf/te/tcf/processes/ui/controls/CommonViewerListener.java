13ea5585181fcddd1bb4d7290f9a5f78eca7c91f
William Chen <william.chen@windriver.com>
2011-12-25 20:45:48 +0800

    Target Explorer: Add license declaration and comments.

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/controls/CommonViewerListener.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.processes.ui/src/org/eclipse/tcf/te/tcf/processes/ui/controls/CommonViewerListener.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Wind River Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Wind River Systems - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.tcf.te.tcf.processes.ui.controls;
 
 import org.eclipse.jface.util.IPropertyChangeListener;
@@ -6,12 +15,27 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.navigator.CommonViewer;
 
+/**
+ * CommonViewerListener listens to the property change event from Target Explorer's
+ *  tree and update the viewer accordingly.
+ */
 public class CommonViewerListener implements IPropertyChangeListener {
+	// The common viewer of Target Explorer view.
 	private CommonViewer viewer;
+	
+	/***
+	 * Create an instance for the specified common viewer.
+	 * 
+	 * @param viewer The common viewer from Target Explorer view.
+	 */
 	public CommonViewerListener(CommonViewer viewer) {
 		this.viewer = viewer;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
+	 */
 	@Override
     public void propertyChange(final PropertyChangeEvent event) {
 		Tree tree = viewer.getTree();
