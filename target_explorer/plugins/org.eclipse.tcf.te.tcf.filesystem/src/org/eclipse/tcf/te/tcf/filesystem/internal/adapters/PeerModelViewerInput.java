5ad8c0468a8608a4bd4fd1ba54dee956f2da0161
William Chen <william.chen@windriver.com>
2012-01-13 17:24:21 +0800

    Target Explorer: Re-parent PeerModelViewerInput with PropertyChangeProvider to simplify the implementation.

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.filesystem/src/org/eclipse/tcf/te/tcf/filesystem/internal/adapters/PeerModelViewerInput.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.filesystem/src/org/eclipse/tcf/te/tcf/filesystem/internal/adapters/PeerModelViewerInput.java
@@ -9,21 +9,14 @@
  *******************************************************************************/
 package org.eclipse.tcf.te.tcf.filesystem.internal.adapters;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.tcf.te.tcf.locator.interfaces.nodes.IPeerModel;
 import org.eclipse.tcf.te.ui.interfaces.IViewerInput;
+import org.eclipse.tcf.te.ui.utils.PropertyChangeProvider;
 
 /**
  * The viewer input of an IPeerModel instance.
  */
-public class PeerModelViewerInput implements IViewerInput {
-	// Property Change Listeners
-	private List<IPropertyChangeListener> propertyChangeListeners;
+public class PeerModelViewerInput extends PropertyChangeProvider implements IViewerInput {
 	// The peer model.
 	private IPeerModel peerModel;
 	
@@ -34,42 +27,6 @@ public class PeerModelViewerInput implements IViewerInput {
 	 */
 	public PeerModelViewerInput(IPeerModel peerModel) {
 		this.peerModel = peerModel;
-		this.propertyChangeListeners = Collections.synchronizedList(new ArrayList<IPropertyChangeListener>());
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.tcf.te.ui.interfaces.IViewerInput#firePropertyChange(org.eclipse.jface.util.PropertyChangeEvent)
-	 */
-	@Override
-    public void firePropertyChange(PropertyChangeEvent event) {
-		synchronized (propertyChangeListeners) {
-			for(IPropertyChangeListener listener : propertyChangeListeners) {
-				listener.propertyChange(event);
-			}
-		}
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.tcf.te.ui.interfaces.IViewerInput#addPropertyChangeListener(org.eclipse.jface.util.IPropertyChangeListener)
-	 */
-	@Override
-    public void addPropertyChangeListener(IPropertyChangeListener listener) {
-		if(!propertyChangeListeners.contains(listener)) {
-			propertyChangeListeners.add(listener);
-		}
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.tcf.te.ui.interfaces.IViewerInput#removePropertyChangeListener(org.eclipse.jface.util.IPropertyChangeListener)
-	 */
-	@Override
-    public void removePropertyChangeListener(IPropertyChangeListener listener) {
-		if(propertyChangeListeners.contains(listener)) {
-			propertyChangeListeners.remove(listener);
-		}
 	}
 
 	/*
