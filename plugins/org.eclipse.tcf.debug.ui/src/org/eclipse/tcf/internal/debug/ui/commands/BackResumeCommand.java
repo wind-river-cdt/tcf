cfad33e60a8d5ff457bbf29014c827375abdb05b
Eugene Tarassov <eugene.tarassov@windriver.com>
2012-03-13 17:23:05 -0700

    TCF Debugger: better views update scheduling on Resume action.

--- plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/commands/BackResumeCommand.java
+++ plugins/org.eclipse.tcf.debug.ui/src/org/eclipse/tcf/internal/debug/ui/commands/BackResumeCommand.java
@@ -14,12 +14,16 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.commands.IDebugCommandRequest;
 import org.eclipse.tcf.internal.debug.actions.TCFAction;
+import org.eclipse.tcf.internal.debug.model.TCFContextState;
 import org.eclipse.tcf.internal.debug.ui.Activator;
 import org.eclipse.tcf.internal.debug.ui.model.TCFModel;
+import org.eclipse.tcf.internal.debug.ui.model.TCFNode;
+import org.eclipse.tcf.internal.debug.ui.model.TCFNodeExecContext;
 import org.eclipse.tcf.protocol.IChannel;
 import org.eclipse.tcf.protocol.IErrorReport;
 import org.eclipse.tcf.protocol.IToken;
 import org.eclipse.tcf.services.IRunControl;
+import org.eclipse.tcf.util.TCFDataCache;
 
 public class BackResumeCommand extends StepCommand {
 
@@ -36,14 +40,25 @@ public class BackResumeCommand extends StepCommand {
     protected void execute(final IDebugCommandRequest monitor,
             final IRunControl.RunControlContext ctx, boolean src_step, final Runnable done) {
         new TCFAction(model.getLaunch(), ctx.getID()) {
+            boolean cmd_sent;
             public void run() {
+                if (cmd_sent) {
+                    TCFNode node = model.getNode(ctx_id);
+                    if (node instanceof TCFNodeExecContext) {
+                        TCFDataCache<TCFContextState> state = ((TCFNodeExecContext)node).getState();
+                        if (!state.validate(this)) return;
+                    }
+                    done();
+                    return;
+                }
+                cmd_sent = true;
                 ctx.resume(IRunControl.RM_REVERSE_RESUME, 1, new IRunControl.DoneCommand() {
                     public void doneCommand(IToken token, Exception error) {
                         if (error != null && model.getChannel().getState() == IChannel.STATE_OPEN) {
                             if (error instanceof IErrorReport) {
                                 IErrorReport r = (IErrorReport)error;
                                 if (r.getErrorCode() == IErrorReport.TCF_ERROR_ALREADY_RUNNING) {
-                                    done();
+                                    run();
                                     return;
                                 }
                             }
@@ -51,7 +66,7 @@ public class BackResumeCommand extends StepCommand {
                             monitor.setStatus(new Status(IStatus.ERROR,
                                     Activator.PLUGIN_ID, IStatus.OK, "Cannot resume: " + error.getLocalizedMessage(), error));
                         }
-                        done();
+                        run();
                     }
                 });
             }
