8969c3152914295933c8f39d3f3f365f8d6613d1
William Chen <william.chen@windriver.com>
2012-03-31 10:45:18 +0800

    Target Explorer: Use IConfirmCallback to intercept confirmation for testing purpose.

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.filesystem/src/org/eclipse/tcf/te/tcf/filesystem/internal/adapters/DeleteHandlerDelegate.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.filesystem/src/org/eclipse/tcf/te/tcf/filesystem/internal/adapters/DeleteHandlerDelegate.java
@@ -20,6 +20,7 @@ import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.tcf.te.runtime.interfaces.callback.ICallback;
 import org.eclipse.tcf.te.runtime.interfaces.properties.IPropertiesContainer;
+import org.eclipse.tcf.te.tcf.filesystem.interfaces.IConfirmCallback;
 import org.eclipse.tcf.te.tcf.filesystem.internal.operations.FSDelete;
 import org.eclipse.tcf.te.tcf.filesystem.model.FSTreeNode;
 import org.eclipse.tcf.te.tcf.filesystem.nls.Messages;
@@ -29,12 +30,30 @@ import org.eclipse.ui.PlatformUI;
 /**
  * File System tree node delete handler delegate implementation.
  */
-public class DeleteHandlerDelegate implements IDeleteHandlerDelegate {
+public class DeleteHandlerDelegate implements IDeleteHandlerDelegate, IConfirmCallback {
 
 	// The key to access the selection in the state.
 	private static final String KEY_SELECTION = "selection"; //$NON-NLS-1$
 	// The key to access the processed state in the state.
 	private static final String KEY_PROCESSED = "processed"; //$NON-NLS-1$
+	// The confirmation callback
+	private IConfirmCallback confirmCallback;
+	
+	/**
+	 * Constructor
+	 */
+	public DeleteHandlerDelegate() {
+		confirmCallback = this;
+	}
+	
+	/**
+	 * Set the confirmation callback
+	 * 
+	 * @param confirmCallback The confirmation callback
+	 */
+	public void setConfirmCallback(IConfirmCallback confirmCallback) {
+		this.confirmCallback = confirmCallback;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -64,22 +83,46 @@ public class DeleteHandlerDelegate implements IDeleteHandlerDelegate {
 		UUID lastProcessed = (UUID) state.getProperty(KEY_PROCESSED);
 		if (lastProcessed == null || !lastProcessed.equals(state.getUUID())) {
 			state.setProperty(KEY_PROCESSED, state.getUUID());
-			IStructuredSelection selection = (IStructuredSelection) state.getProperty(KEY_SELECTION);
-			List<FSTreeNode> nodes = selection.toList();
-			String question;
-			if (nodes.size() == 1) {
-				FSTreeNode node = nodes.get(0);
-				question = NLS.bind(Messages.DeleteFilesHandler_DeleteOneFileConfirmation, node.name);
-			}
-			else {
-				question = NLS.bind(Messages.DeleteFilesHandler_DeleteMultipleFilesConfirmation, Integer.valueOf(nodes.size()));
-			}
-			Shell parent = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-			if (MessageDialog.openQuestion(parent, Messages.DeleteFilesHandler_ConfirmDialogTitle, question)) {
-				FSDelete delete = new FSDelete(nodes);
-				delete.doit();
+			if(confirmCallback != null) {
+				IStructuredSelection selection = (IStructuredSelection) state.getProperty(KEY_SELECTION);
+				if(confirmCallback.requires(selection) && confirmCallback.confirms(selection) == IConfirmCallback.YES) {
+					List<FSTreeNode> nodes = selection.toList();
+					FSDelete delete = new FSDelete(nodes);
+					delete.doit();
+				}
 			}
 		}
 		if (callback != null) callback.done(this, Status.OK_STATUS);
 	}
+
+	/*
+	 * 
+	 */
+	@Override
+    public boolean requires(Object object) {
+	    return true;
+    }
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.tcf.te.tcf.filesystem.interfaces.IConfirmCallback#confirms(java.lang.Object)
+	 */
+	@Override
+    public int confirms(Object object) {
+		IStructuredSelection selection = (IStructuredSelection) object;
+		List<FSTreeNode> nodes = selection.toList();			
+		String question;
+		if (nodes.size() == 1) {
+			FSTreeNode node = nodes.get(0);
+			question = NLS.bind(Messages.DeleteFilesHandler_DeleteOneFileConfirmation, node.name);
+		}
+		else {
+			question = NLS.bind(Messages.DeleteFilesHandler_DeleteMultipleFilesConfirmation, Integer.valueOf(nodes.size()));
+		}
+		Shell parent = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		if (MessageDialog.openQuestion(parent, Messages.DeleteFilesHandler_ConfirmDialogTitle, question)) { 
+			return IConfirmCallback.YES;
+		}
+		return IConfirmCallback.NO;
+    }
 }
