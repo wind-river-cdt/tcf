8739cdba5550a829cefe9ee8a5ec2af6843d31b8
Uwe Stieber <uwe.stieber@windriver.com>
2012-04-17 16:53:09 +0200

    Target Explorer: code cleanup, removed commented out code

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/internal/adapters/PeerPersistableURIProvider.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/internal/adapters/PeerPersistableURIProvider.java
@@ -9,32 +9,30 @@
  *******************************************************************************/
 package org.eclipse.tcf.te.tcf.locator.internal.adapters;
 
-import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.tcf.core.TransientPeer;
 import org.eclipse.tcf.protocol.IPeer;
-import org.eclipse.tcf.protocol.JSON;
 import org.eclipse.tcf.protocol.Protocol;
 import org.eclipse.tcf.te.core.adapters.ModelNodePersistableURIProvider;
-import org.eclipse.tcf.te.runtime.model.interfaces.IModelNode;
 import org.eclipse.tcf.te.runtime.persistence.interfaces.IPersistableNodeProperties;
 import org.eclipse.tcf.te.tcf.locator.interfaces.nodes.IPeerModel;
 import org.eclipse.tcf.te.tcf.locator.interfaces.nodes.IPeerModelProvider;
-import org.eclipse.tcf.te.tcf.locator.interfaces.services.ILocatorModelLookupService;
-import org.eclipse.tcf.te.tcf.locator.model.Model;
 import org.eclipse.tcf.te.tcf.locator.model.ModelLocationUtil;
-import org.eclipse.tcf.te.tcf.locator.nodes.PeerModel;
 
 /**
  * Persistable implementation handling peer attributes.
  */
 public class PeerPersistableURIProvider extends ModelNodePersistableURIProvider {
 
+	/**
+	 * Determine the peer from the given context object.
+	 *
+	 * @param context The context object or <code>null</code>.
+	 * @return The peer or <code>null</code>.
+	 */
 	private IPeer getPeer(Object context) {
 		IPeer peer = null;
 
@@ -107,102 +105,5 @@ public class PeerPersistableURIProvider extends ModelNodePersistableURIProvider 
 	@Override
 	public IPath getRoot() {
 		return ModelLocationUtil.getStaticPeersRootLocation();
-	}
-
-	public String exportStringFrom(final Object data) {
-		final AtomicReference<String> encoded = new AtomicReference<String>();
-
-		Runnable runnable = new Runnable() {
-			@Override
-			public void run() {
-				//				try {
-				//					Map<String, Object> attrs = exportFrom(data);
-				//
-				//					// Remove all transient attributes
-				//					String[] keys = attrs.keySet().toArray(new String[attrs.keySet().size()]);
-				//					for (String key : keys) {
-				//						if (key.endsWith(".transient")) { //$NON-NLS-1$
-				//							attrs.remove(key);
-				//						}
-				//					}
-				//
-				//					encoded.set(JSON.toJSON(attrs));
-				//				}
-				//				catch (IOException e) {
-				//					if (Platform.inDebugMode()) {
-				//						IStatus status = new Status(IStatus.ERROR, CoreBundleActivator.getUniqueIdentifier(),
-				//										"PeerPersistableURIProvider export failure: " + e.getLocalizedMessage(), e); //$NON-NLS-1$
-				//						Platform.getLog(CoreBundleActivator.getDefault().getBundle()).log(status);
-				//					}
-				//				}
-			}
-		};
-
-		if (Protocol.isDispatchThread()) {
-			runnable.run();
-		}
-		else {
-			Protocol.invokeAndWait(runnable);
-		}
-
-		return encoded.get() != null ? encoded.get() : ""; //$NON-NLS-1$
-	}
-
-	public Object importFrom(final String external) throws IOException {
-		Assert.isNotNull(external);
-
-		final AtomicReference<IPeerModel> decoded = new AtomicReference<IPeerModel>();
-		final AtomicReference<IOException> error = new AtomicReference<IOException>();
-
-		Runnable runnable = new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Object o = JSON.parseOne(external.getBytes("UTF-8")); //$NON-NLS-1$
-					// The decoded object should be a map
-					if (o instanceof Map) {
-						@SuppressWarnings("unchecked")
-						Map<String, String> attrs = (Map<String, String>)o;
-
-						// Get the id of the decoded attributes
-						String id = attrs.get("ID"); //$NON-NLS-1$
-						if (id == null) {
-							throw new IOException("PeerPersistableURIProvider#import: Mandatory attribure 'ID' is missing."); //$NON-NLS-1$
-						}
-
-						// Lookup the id within the model
-						IPeerModel candidate = Model.getModel().getService(ILocatorModelLookupService.class).lkupPeerModelById(id);
-						if (candidate != null) {
-							decoded.set(candidate);
-							return;
-						}
-
-						// Not found in the model -> create a ghost object
-						IPeer peer = new TransientPeer(attrs);
-						IPeerModel peerModel = new PeerModel(Model.getModel(), peer);
-						peerModel.setProperty(IModelNode.PROPERTY_IS_GHOST, true);
-
-						decoded.set(peerModel);
-					} else {
-						throw new IOException("PeerPersistableURIProvider#import: Object not of map type."); //$NON-NLS-1$
-					}
-				} catch (IOException e) {
-					error.set(e);
-				}
-			}
-		};
-
-		if (Protocol.isDispatchThread()) {
-			runnable.run();
-		}
-		else {
-			Protocol.invokeAndWait(runnable);
-		}
-
-		if (error.get() != null) {
-			throw error.get();
-		}
-
-		return decoded.get() != null ? decoded.get() : ""; //$NON-NLS-1$
 	}
 }
