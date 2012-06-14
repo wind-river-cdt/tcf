74f53f7657b1b1b940d488a626c43dee26bcb8ae
Uwe Stieber <uwe.stieber@windriver.com>
2012-06-14 07:15:08 +0200

    Target Explorer: Avoid scanning of neighborhood if on same host or in same network

--- target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/ScannerRunnable.java
+++ target_explorer/plugins/org.eclipse.tcf.te.tcf.locator/src/org/eclipse/tcf/te/tcf/locator/ScannerRunnable.java
@@ -146,137 +146,156 @@ public class ScannerRunnable implements Runnable, IChannel.IChannelListener {
 			// Keep the channel open as long as the query for the remote peers is running.
 			boolean keepOpen = false;
 
-			// Get the parent model from the model mode
-			final ILocatorModel model = (ILocatorModel)peerNode.getAdapter(ILocatorModel.class);
-			if (model != null) {
-				// Use the open channel to ask the remote peer what other peers it knows
-				ILocator locator = channel.getRemoteService(ILocator.class);
-				if (locator != null) {
-					// Channel must be kept open as long as the command runs
-					keepOpen = true;
-					// Issue the command
-		            new Command(channel, locator, "getPeers", null) { //$NON-NLS-1$
-		                @Override
-		                public void done(Exception error, Object[] args) {
-		                    if (error == null) {
-		                        Assert.isTrue(args.length == 2);
-		                        error = toError(args[0]);
-		                    }
-		                    // If the error is still null here, process the returned peers
-		                    if (error == null && args[1] != null) {
-	                        	// Get the parent peer
-	                        	IPeer parentPeer = channel.getRemotePeer();
-	                        	// Get the old child list
-	                        	List<IPeerModel> oldChildren = new ArrayList<IPeerModel>(model.getChildren(parentPeer.getID()));
+			// If we don't queried the DNS name of the peer, or the peer IP changed,
+			// trigger a query (can run in any thread, outside TCF dispatch and UI
+			// thread). This make sense only if there is an IP address to query at all.
+			final String ip = channel.getRemotePeer().getAttributes().get(IPeer.ATTR_IP_HOST);
+			if (ip != null && !"".equals(ip)) { //$NON-NLS-1$
+				if (peerNode.getStringProperty("dns.name.transient") == null || !ip.equals(peerNode.getStringProperty("dns.lastIP.transient"))) { //$NON-NLS-1$ //$NON-NLS-2$
+					// If the IP address changed, reset the "do not query again" marker
+					if (!ip.equals(peerNode.getStringProperty("dns.lastIP.transient"))) { //$NON-NLS-1$
+						peerNode.setProperty("dns.lastIP.transient", ip); //$NON-NLS-1$
+						peerNode.setProperty("dns.skip.transient", false); //$NON-NLS-1$
+					}
 
-		                    	// "getPeers" returns a collection of peer attribute maps
-		                        @SuppressWarnings("unchecked")
-                                Collection<Map<String,String>> peerAttributesList = (Collection<Map<String,String>>)args[1];
-		                        for (Map<String,String> attributes : peerAttributesList) {
-		                        	// Get the peer id
-		                        	String peerId = attributes.get(IPeer.ATTR_ID);
-		                        	// Create a peer instance
-		                        	IPeer peer = new PeerRedirector(parentPeer, attributes);
-									// Try to find an existing peer node first
-									IPeerModel peerNode = model.getService(ILocatorModelLookupService.class).lkupPeerModelById(parentPeer.getID(), peerId);
-									if (peerNode == null) {
-										// Not yet known -> add it
-										peerNode = new PeerModel(model, peer);
-										peerNode.setParent(ScannerRunnable.this.peerNode);
-										// Validate the peer node before adding
-										peerNode = model.validateChildPeerNodeForAdd(peerNode);
-										if (peerNode != null) {
-											// Add the child peer node to model
-											model.getService(ILocatorModelUpdateService.class).addChild(peerNode);
-											// And schedule for immediate status update
-											Runnable runnable = new ScannerRunnable(getParentScanner(), peerNode);
-											Protocol.invokeLater(runnable);
+					if (!peerNode.getBooleanProperty("dns.skip.transient")) { //$NON-NLS-1$
+						Runnable runnable = new Runnable() {
+							@Override
+							public void run() {
+								try {
+									InetAddress address = InetAddress.getByName(ip);
+									final AtomicReference<String> nameRef = new AtomicReference<String>();
+									nameRef.set(address.getCanonicalHostName());
+
+									if (ip.equals(nameRef.get()) && IPAddressUtil.getInstance().isLocalHost(ip)) {
+										String[] candidates = IPAddressUtil.getInstance().getCanonicalHostNames();
+										for (String candidate : candidates) {
+											if (!ip.equals(candidate)) {
+												nameRef.set(candidate);
+												break;
+											}
 										}
-									} else {
-										// The parent node should be set and match
-										Assert.isTrue(peerNode.getParent(IPeerModel.class) != null && peerNode.getParent(IPeerModel.class).equals(ScannerRunnable.this.peerNode));
-										// Peer node found, update the peer instance
-										peerNode.setProperty(IPeerModelProperties.PROP_INSTANCE, peer);
-										// And remove it from the old child list
-										oldChildren.remove(peerNode);
 									}
-		                        }
 
-		                        // Everything left in the old child list is not longer known to the remote peer
-		                        // However, the child list may include manual redirected static peers. Do not
-		                        // remove them here.
-		                        for (IPeerModel child : oldChildren) {
-		                        	String value = child.getPeer().getAttributes().get("static.transient"); //$NON-NLS-1$
-		                        	if (value == null || !Boolean.parseBoolean(value.trim())) {
-		                        		// Remove the child peer node from the model
-		                        		model.getService(ILocatorModelUpdateService.class).removeChild(child);
-		                        	}
-		                        }
-		                    }
+									Protocol.invokeLater(new Runnable() {
+										@Override
+										public void run() {
+											String name = nameRef.get();
+											if (name != null && !"".equals(name) && !ip.equals(name)) { //$NON-NLS-1$
+												String dnsName = name.indexOf('.') != -1 ? name.substring(0, name.indexOf('.')) : name;
+												if (!ip.equalsIgnoreCase(dnsName)) {
+													peerNode.setProperty("dns.name.transient", dnsName.toLowerCase()); //$NON-NLS-1$
+												}
+											}
+										}
+									});
+								}
+								catch (UnknownHostException e) {
+									Protocol.invokeLater(new Runnable() {
+										@Override
+                                        public void run() {
+											peerNode.setProperty("dns.skip.transient", true); //$NON-NLS-1$
+										}
+									});
+								}
+							}
+						};
 
-		                    // Once everything is processed, close the channel
-		                    if (!sharedChannel) channel.close();
-		                }
-		            };
+						Thread thread = new Thread(runnable, "DNS Query Thread for " + ip); //$NON-NLS-1$
+						thread.start();
+					}
+				}
+			}
+
+			// Don't ask for discovered peers on the same host or in the same network
+			if (ip != null && !"".equals(ip) && !IPAddressUtil.getInstance().isLocalHost(ip)) { //$NON-NLS-1$
+				boolean sameNet = false;
+				String net = ip.substring(0, ip.lastIndexOf('.'));
+				String[] addresses = IPAddressUtil.getInstance().getLocalHostAddresses(IPAddressUtil.HOSTMAP_ANY_UNICAST
+																							| IPAddressUtil.HOSTMAP_CANONICALADDR
+																							| IPAddressUtil.HOSTMAP_LOOPBACK
+																							| IPAddressUtil.HOSTMAP_ADDR
+																							| IPAddressUtil.HOSTMAP_IPV4);
+				for (String address : addresses) {
+					if (address.startsWith(net)) {
+						sameNet = true;
+						break;
+					}
 				}
 
-				// If we don't queried the DNS name of the peer, or the peer IP changed,
-				// trigger a query (can run in any thread, outside TCF dispatch and UI
-				// thread). This make sense only if there is an IP address to query at all.
-				final String ip = channel.getRemotePeer().getAttributes().get(IPeer.ATTR_IP_HOST);
-				if (ip != null && !"".equals(ip)) { //$NON-NLS-1$
-					if (peerNode.getStringProperty("dns.name.transient") == null || !ip.equals(peerNode.getStringProperty("dns.lastIP.transient"))) { //$NON-NLS-1$ //$NON-NLS-2$
-						// If the IP address changed, reset the "do not query again" marker
-						if (!ip.equals(peerNode.getStringProperty("dns.lastIP.transient"))) { //$NON-NLS-1$
-							peerNode.setProperty("dns.lastIP.transient", ip); //$NON-NLS-1$
-							peerNode.setProperty("dns.skip.transient", false); //$NON-NLS-1$
-						}
-
-						if (!peerNode.getBooleanProperty("dns.skip.transient")) { //$NON-NLS-1$
-							Runnable runnable = new Runnable() {
+				if (!sameNet) {
+					// Get the parent model from the model mode
+					final ILocatorModel model = (ILocatorModel)peerNode.getAdapter(ILocatorModel.class);
+					if (model != null) {
+						// Use the open channel to ask the remote peer what other peers it knows
+						ILocator locator = channel.getRemoteService(ILocator.class);
+						if (locator != null) {
+							// Channel must be kept open as long as the command runs
+							keepOpen = true;
+							// Issue the command
+							new Command(channel, locator, "getPeers", null) { //$NON-NLS-1$
 								@Override
-								public void run() {
-									try {
-										InetAddress address = InetAddress.getByName(ip);
-										final AtomicReference<String> nameRef = new AtomicReference<String>();
-										nameRef.set(address.getCanonicalHostName());
+								public void done(Exception error, Object[] args) {
+									if (error == null) {
+										Assert.isTrue(args.length == 2);
+										error = toError(args[0]);
+									}
+									// If the error is still null here, process the returned peers
+									if (error == null && args[1] != null) {
+										// Get the parent peer
+										IPeer parentPeer = channel.getRemotePeer();
+										// Get the old child list
+										List<IPeerModel> oldChildren = new ArrayList<IPeerModel>(model.getChildren(parentPeer.getID()));
 
-										if (ip.equals(nameRef.get()) && IPAddressUtil.getInstance().isLocalHost(ip)) {
-											String[] candidates = IPAddressUtil.getInstance().getCanonicalHostNames();
-											for (String candidate : candidates) {
-												if (!ip.equals(candidate)) {
-													nameRef.set(candidate);
-													break;
+										// "getPeers" returns a collection of peer attribute maps
+										@SuppressWarnings("unchecked")
+										Collection<Map<String,String>> peerAttributesList = (Collection<Map<String,String>>)args[1];
+										for (Map<String,String> attributes : peerAttributesList) {
+											// Get the peer id
+											String peerId = attributes.get(IPeer.ATTR_ID);
+											// Create a peer instance
+											IPeer peer = new PeerRedirector(parentPeer, attributes);
+											// Try to find an existing peer node first
+											IPeerModel peerNode = model.getService(ILocatorModelLookupService.class).lkupPeerModelById(parentPeer.getID(), peerId);
+											if (peerNode == null) {
+												// Not yet known -> add it
+												peerNode = new PeerModel(model, peer);
+												peerNode.setParent(ScannerRunnable.this.peerNode);
+												// Validate the peer node before adding
+												peerNode = model.validateChildPeerNodeForAdd(peerNode);
+												if (peerNode != null) {
+													// Add the child peer node to model
+													model.getService(ILocatorModelUpdateService.class).addChild(peerNode);
+													// And schedule for immediate status update
+													Runnable runnable = new ScannerRunnable(getParentScanner(), peerNode);
+													Protocol.invokeLater(runnable);
 												}
+											} else {
+												// The parent node should be set and match
+												Assert.isTrue(peerNode.getParent(IPeerModel.class) != null && peerNode.getParent(IPeerModel.class).equals(ScannerRunnable.this.peerNode));
+												// Peer node found, update the peer instance
+												peerNode.setProperty(IPeerModelProperties.PROP_INSTANCE, peer);
+												// And remove it from the old child list
+												oldChildren.remove(peerNode);
 											}
 										}
 
-										Protocol.invokeLater(new Runnable() {
-											@Override
-											public void run() {
-												String name = nameRef.get();
-												if (name != null && !"".equals(name) && !ip.equals(name)) { //$NON-NLS-1$
-													String dnsName = name.indexOf('.') != -1 ? name.substring(0, name.indexOf('.')) : name;
-													if (!ip.equalsIgnoreCase(dnsName)) {
-														peerNode.setProperty("dns.name.transient", dnsName.toLowerCase()); //$NON-NLS-1$
-													}
-												}
+										// Everything left in the old child list is not longer known to the remote peer
+										// However, the child list may include manual redirected static peers. Do not
+										// remove them here.
+										for (IPeerModel child : oldChildren) {
+											String value = child.getPeer().getAttributes().get("static.transient"); //$NON-NLS-1$
+											if (value == null || !Boolean.parseBoolean(value.trim())) {
+												// Remove the child peer node from the model
+												model.getService(ILocatorModelUpdateService.class).removeChild(child);
 											}
-										});
+										}
 									}
-									catch (UnknownHostException e) {
-										Protocol.invokeLater(new Runnable() {
-											@Override
-                                            public void run() {
-												peerNode.setProperty("dns.skip.transient", true); //$NON-NLS-1$
-											}
-										});
-									}
+
+									// Once everything is processed, close the channel
+									if (!sharedChannel) channel.close();
 								}
 							};
-
-							Thread thread = new Thread(runnable, "DNS Query Thread for " + ip); //$NON-NLS-1$
-							thread.start();
 						}
 					}
 				}
